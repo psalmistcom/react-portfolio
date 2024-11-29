425f94dee63ff034f0561b68020b54857522fd70
psalmistcom <dadewale9@gmail.com>
2024-11-29 22:00:28 +0100

    updates

--- src/content_option.js
+++ src/content_option.js
@@ -1,15 +1,15 @@
-const logotext = "JOHN";
+const logotext = "DAVID ADEWALE";
 const meta = {
-    title: "John Doe",
-    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+    title: "David Adewale",
+    description: "I’m David Adewale - A Full stack developer with Laravel PHP, ReactJS, Typescript, InertiaJS & other technologies.",
 };
 
 const introdata = {
-    title: "I’m John Doe",
+    title: "I’m David Adewale",
     animated: {
         first: "I love coding",
         second: "I code cool websites",
-        third: "I develop mobile apps",
+        third: "I develop web apps",
     },
     description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum",
     your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
